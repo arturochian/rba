6d5f6a410d01cba8111630d72a09bcb75a21a193
Arturo <arturob.chian@gmail.com>
2014-08-05 16:58:58 -0500

    practise

--- global0.R
+++ global0.R
@@ -9,5 +9,21 @@ sourceDir <- function(path, trace = TRUE, ...) {
   }
 }
 library(R6)
+library(data.table)
 source("TimeSeries.R")
 sourceDir(getwd())
+#?R6Class
+
+TimeSeries$data<-OHLCV
+class(TimeSeries$data)
+Returns$calcAlpha$OHLCV
+return()
+TimeSeries$new()
+Prices$new(data=OHLCV)
+Returns$new
+$data<-OHLCV
+devtools::install_github('wch/R6',build_vignettes = FALSE)
+
+sessionInfo()
+OHLCV<-Returns$new
+OHLCV
